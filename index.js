e2eda7d339a535c9bbc674555e4ea749ebdc9b5b
Esbon Omwenga <victoromwenga365@gmail.com>
2022-12-01 16:50:31 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const messages = [];
+function writeCards(names,eventname){
+  for(let counter=0;counter<names.length;counter=counter+1){
+    console.log (messages.push(`Thank you, ${names[counter]}, for the wonderful surprise gift!`))
+  }
+  return messages;
+}
+function countDown() {
+let number = 10;
+while(number>-1){
+  
+  console.log(number);
+  number=number-1;
+
+}
+
+}
+
+
